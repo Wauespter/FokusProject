f8020aba8e54e68678fb147c88567fc90c3e4269
WAUESPTER ABICH SOUZA <146996798+Wauespter@users.noreply.github.com>
2024-07-01 18:58:12 -0300

    Update script.js
    Update Scrip

--- script.js
+++ script.js
@@ -88,6 +88,11 @@ const contagemRegressiva = () => {
     if(tempoDecorridoEmSegundos <= 0){
         somBeep.play()
         alert('Tempo finalizado!')
+        const focoAtivo = html.getAttribute('data-contexto') == 'foco'
+        if (focoAtivo) {
+            const evento = new CustomEvent('FocoFinalizado')
+            document.dispatchEvent(evento)
+        }
         zerar()
         tempoDecorridoEmSegundos = resetTempoDecorridoEmSegundos;
         mostrarTempo()
